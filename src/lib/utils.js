0c796f7d3a7a3502a5b3253ee925bf9cdb52133a
raffleberry <raffleberry42@gmail.com>
2023-02-23 22:56:56 +0530

    cleanup

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,4 @@
 function changeSrc(src) {
-    console.log(src);
     localStorage.setItem('src', src);
     var fileName = require('path').parse(src).base;
     localStorage.setItem('title', fileName);
